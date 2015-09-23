c2bff870f8c977fc2de9781736e5693c7fbe1974
Antonin DENIAU <antonin.deniau@gmail.com>
2015-09-23 20:29:30 +0200

    add-raw-option

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ var loaderUtils = require('loader-utils');
 var dsv = require('dsv');
 
 module.exports = function(text) {
+  var query = loaderUtils.parseQuery(this.query);
+  var delimiter = query.delimiter || ',';
+  var parser = dsv(delimiter);
 
-  var query = loaderUtils.parseQuery(this.query),
-      delimiter = query.delimiter || ',',
-      parser = dsv(delimiter),
-      res = parser.parse(text);
+  if ( query.raw ) {
+      var res = parser.parseRows(text);
+  } else {
+      var res = parser.parse(text);
+  }
 
   return 'module.exports = ' + JSON.stringify(res);
 }
